eaeb397a118416e70838a2d94684f55be2f753b9
SluzCode <17539585+SluzCode@users.noreply.github.com>
2023-09-07 16:13:57 -0700

    Add additional solns to js exercise
    This contains the solutions to exercise 5, 6, and 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(low,high) {
+
+if ((high < 0||low < 0)||(typeof low !== 'number')||(typeof high !== 'number'))
+{
+return "ERROR"}
+
+// 
+    let sum = 0;
+    let range = Math.abs(high - low);
+
+for (let i = 0; i < range+1; i++) {
+
+       sum += (high+low)-(range+1) + i;
+
+    //    console.log(sum);
+}
+
+return sum;
 
 };
+
+
+sumAll(10,1);
 
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+
+let tempC = (tempF - 32) * (5/9);
+
+return parseFloat(tempC.toFixed(1));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+
+let tempF = (9/5) * tempC + 32;
+
+return parseFloat(tempF.toFixed(1));
+
 };
+
+
+convertToCelsius(100);
+convertToFahrenheit(25);
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+
